c7735864d32ca0ddea5a41e5af764dd60c2a8375
OwenB523 <buckleow@tcd.ie>
2020-11-09 21:18:49 +0100

    Updating LCA to work with DAG

--- LCA_Python.py
+++ LCA_Python.py
@@ -1,79 +1,95 @@
-# Python Program for Lowest Common Ancestor in a Binary Tree
-# O(n) solution to find LCS of two given values n1 and n2
+class Node(object):
 
-# A binary tree node
-class Node:
-	# Constructor to create a new binary node
-	def __init__(self, key):
-		self.key = key
-		self.left = None
-		self.right = None
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
-# Finds the path from root node to given root of the tree.
-# Stores the path in a list path[], returns true if path
-# exists otherwise false
-def findPath( root, path, k):
+    def __lt__(self, key):
+        return self.key < key
 
-	# Baes Case
-	if root is None:
-		return False
+    def __gt__(self, key):
+        return self.key > key
 
-	# Store this node is path vector. The node will be
-	# removed if not in path from root to k
-	path.append(root.key)
-
-	# See if the k is same as root's key
-	if root.key == k :
-		return True
-
-	# Check if k is found in left or right sub-tree
-	if ((root.left != None and findPath(root.left, path, k)) or
-			(root.right!= None and findPath(root.right, path, k))):
-		return True
-
-	# If not present in subtree rooted with root, remove
-	# root from path and return False
-
-	path.pop()
-	return False
-
-# Returns LCA if node n1 , n2 are present in the given
-# binary tre otherwise return -1
-def findLCA(root, n1, n2):
-
-	# To store paths to n1 and n2 fromthe root
-	path1 = []
-	path2 = []
-
-	# Find paths from root to n1 and root to n2.
-	# If either n1 or n2 is not present , return -1
-	if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
-		return -1
-
-	# Compare the paths to get the first different value
-	i = 0
-	while(i < len(path1) and i < len(path2)):
-		if path1[i] != path2[i]:
-			break
-		i += 1
-	return path1[i-1]
+    def __eq__(self, key):
+        return self.key == key
 
 
-# Driver program to test above function
-# Let's create the Binary Tree shown in above diagram
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
+class Tree(object):
 
-root45 = (findLCA(root, 4, 5,))
-root46 = (findLCA(root, 4, 6,))
-root34 = (findLCA(root, 3, 4,))
-root24 = (findLCA(root, 2, 4,))
-print ('LCA(4, 5) = ' + str(root45))
-print ('LCA(4, 6) = ' + str(root46))
-print ('LCA(3, 4) = ' + str(root34))
-print ('LCA(2, 4) = ' + str(root24))
+    def __init__(self):
+        self.root = None
+
+    def put(self, key):
+        if not (self.node_check(key)):
+            self.root = self._put(self.root, key)
+
+    def _put(self, node, key):
+        if node is None:
+            node = Node(key)
+
+        if key < node:
+            node.left = self._put(node.left, key)
+        elif key > node:
+            node.right = self._put(node.right, key)
+        else:
+            node.key = key
+
+        return node
+
+    def get(self, key):
+        return self._get(self.root, key)
+
+    def _get(self, node, key):
+        while node is not None:
+            if key < node:
+                node = node.left
+            elif key > node:
+                node = node.right
+            else:
+                return node.key
+        return None
+
+    def find_LCA(self, a, b):
+        if a == b:
+            return
+        else:
+            return self._find_LCA(self.root, a, b)
+
+    def _find_LCA(self, node, a, b):
+        if node is None:
+            return None
+        if a > node and b > node:
+            if node.right is None:
+                return None
+            if node.right == a or node.right == b:
+                return node.key
+
+            return self._find_LCA(node.right, a, b)
+
+        elif a < node and b < node:
+            if node.left is None:
+                return None
+
+            if node.left == a or node.left == b:
+                return node.key
+
+            return self._find_LCA(node.left, a, b)
+
+        elif a == self.root or b == self.root:
+            return None
+
+        else:
+            if self._node_check(node, a):
+                if self._node_check(node, b):
+                    return node.key
+                else:
+                    return None
+            else:
+                return None
+
+    def node_check(self, key):
+        return self._node_check(self.root, key)
+
+    def _node_check(self, node, key):
+        return not self._get(node, key) is None
